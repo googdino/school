9132107184e004d6921637ea2935f189ca4f5c74
googdino <100561527+googdino@users.noreply.github.com>
2022-09-23 20:19:33 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
+let num = 0
+input.onGesture(Gesture.Shake, function () {
+    num = randint(1, 3)
+    if (num == 1) {
+    	
+    } else if (num == 2) {
+    	
+    } else if (num == 3) {
+    	
+    }
 })
