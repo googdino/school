48f794c37253c43e5d6522c6e072fba6b8f9db8f
googdino <100561527+googdino@users.noreply.github.com>
2022-09-27 08:24:02 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,28 @@ let num = 0
 input.onGesture(Gesture.Shake, function () {
     num = randint(1, 3)
     if (num == 1) {
-    	
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            # # # # #
+            . # # # .
+            `)
     } else if (num == 2) {
-    	
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # # # #
+            . # # # .
+            `)
     } else if (num == 3) {
-    	
+        basic.showLeds(`
+            . # # # .
+            . # # # .
+            . # # # .
+            # # # # #
+            . # # # .
+            `)
     }
 })
